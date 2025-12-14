663546ca2c98e23ed8384b7af2c2c46c9caaaa4a
Artem <tema.zaslavskii@gmail.com>
2025-12-14 23:35:23 +0700

    :<)

---
+++ src/gorified/PrimesGenerator.java
@@ -0,0 +1,34 @@
+package gorified;
+import java.util.Iterator;
+
+public class PrimesGenerator implements Iterator<Integer> { private int count;
+    private int current = 2;
+    private int generated = 0;
+
+    public PrimesGenerator(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return generated < count;
+    }
+
+    @Override
+    public Integer next() {
+        while (!isPrime(current)) {
+            current++;
+        }
+        int prime = current;
+        current++;
+        generated++;
+        return prime;
+    }
+
+    private boolean isPrime(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
